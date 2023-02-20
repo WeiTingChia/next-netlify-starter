013c7d7e07e2a020e34e7d958c0e7279efb6c182
thomaswei <thomaswei@mail.apex.com.tw>
2023-02-20 14:26:06 +0800

    navAdd

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Nav from '@components/Nav'
 
 export default function Home() {
   return (
     <div className="container">
+      <Nav></Nav>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
